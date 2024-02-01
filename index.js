2a2d8920d4869166dd945c3491ca7a59c9aefb43
Wbaidur-Rahman <wbaidurrahman2000@gmail.com>
2024-02-01 19:27:08 +0600

    Lesson 1 updated

--- index.js
+++ index.js
@@ -9,20 +9,18 @@
 // dependencies
 const http = require('http');
 const  {handleReqRes} = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments')
+
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000
-};
 
 // create server
 app.createServer = () => {
     const server = http.createServer(handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`Listening to port number : ${app.config.port}`)
+    server.listen(environment.port, () => {
+        console.log(`Listening to port number : ${environment.port}`)
     })
 }
 
